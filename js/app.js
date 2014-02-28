89ea2b6cd0361eae21cf7bd537aeb9101ee5dbd1
Keith Webber <keith.e.webber@gmail.com>
2014-02-28 14:33:38 -0500

    guess a number completed

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-alert('Hey find me to know where to put the javascript');
+function getUserInput()  {
+    var stuff = prompt('Guess a number ');
+    return stuff;
+}
+function outputChoice(size, number) {
+ alert(number + ' was to ' + size);
+}
+
+var randomNumber = Math.floor(Math.random()*11);
+var userInput = getUserInput();
+
+while(userInput != randomNumber) {
+  if(userInput > randomNumber){
+    outputChoice('BIG', userInput);
+    userInput = getUserInput();
+  }
+  else {
+    outputChoice('SMALL', userInput);
+    userInput = getUserInput();
+  }
+}
+
+alert("Congrats!!! the number was " + randomNumber);
