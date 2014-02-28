bb577c7564cc5a4c159bee15e4f49691187c6a6b
Keith Webber <keith.e.webber@gmail.com>
2014-02-28 15:13:52 -0500

    cleaned up

--- js/app.js
+++ js/app.js
@@ -1,23 +1,23 @@
 function getUserInput()  {
-    var stuff = prompt('Guess a number ');
-    return stuff;
-}
-function outputChoice(size, number) {
- alert(number + ' was to ' + size);
+  return prompt('Guess a number ');
 }
 
-var randomNumber = Math.floor(Math.random()*11);
+function outputChoice(size, number) {
+  alert(number + ' was to ' + size);
+}
+
+var randomNumber = Math.floor(Math.random() * 11);
 var userInput = getUserInput();
 
-while(userInput != randomNumber) {
-  if(userInput > randomNumber){
+while (userInput != randomNumber) {
+  if (userInput > randomNumber) {
     outputChoice('BIG', userInput);
-    userInput = getUserInput();
-  }
-  else {
+  } else {
     outputChoice('SMALL', userInput);
-    userInput = getUserInput();
   }
+
+  userInput = getUserInput();
 }
 
 alert("Congrats!!! the number was " + randomNumber);
+
