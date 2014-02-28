265fc017d8a51879c41ca70984aa00059e8bab5d
Keith Webber <keith.e.webber@gmail.com>
2014-02-28 15:33:15 -0500

    jQuery DONE

--- js/app.js
+++ js/app.js
@@ -1,23 +1,27 @@
 function getUserInput()  {
-  return prompt('Guess a number ');
+  return $('#user_guess').val();
 }
 
 function outputChoice(size, number) {
-  alert(number + ' was to ' + size);
+  $('#output_area').text(number + ' was to ' + size);
 }
 
 var randomNumber = Math.floor(Math.random() * 11);
-var userInput = getUserInput();
 
-while (userInput != randomNumber) {
-  if (userInput > randomNumber) {
-    outputChoice('BIG', userInput);
-  } else {
-    outputChoice('SMALL', userInput);
-  }
 
-  userInput = getUserInput();
-}
+$('#submit_guess').click(function(e){
+  e.preventDefault();
+  var userInput = parseInt(getUserInput());
 
-alert("Congrats!!! the number was " + randomNumber);
+    if (userInput > randomNumber) {
+      outputChoice('BIG', userInput);
+    } else if (userInput < randomNumber)  {
+      outputChoice('SMALL', userInput);
+    } else {
+      alert("Congrats!!! the number was " + randomNumber);
+    }
+
+});
+
+
 
